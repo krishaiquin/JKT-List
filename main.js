6b8d158f3454324120d55fdc27b718cacd376d91
araxione <lml.judyy@gmail.com>
2021-11-06 15:29:11 -0700

    add checkmark icons without text boxes

---
+++ main.js
@@ -0,0 +1,23 @@
+const todoInput = document.querySelector(".todo-input");
+const todoAddButton = document.querySelector(".todo-add-button");
+const todoList = document.querySelector(".todo-list");
+todoAddButton.addEventListener("click", addTodo);
+
+function addTodo(event) {
+    event.preventDefault();
+
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add("todo");
+    const newTodo = document.createElement('li');
+    newTodo.innerText = "hey";
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+
+    const completedButton = document.createElement("button");
+    completedButton.innerHTML = '<i class="far fa-check-square"></i>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+
+    todoList.appendChild(todoDiv);
+
+}
