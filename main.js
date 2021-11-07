1b42635e65ef324cdfa1cea9c5735040ecb31640
krisha <krishaiquin@gmail.com>
2021-11-06 17:22:55 -0700

    add button works now, addTodo checks if entry already exists

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+
+var items = [];
 const todoInput = document.querySelector(".todo-input");
 const todoAddButton = document.querySelector(".todo-add-button");
 const todoList = document.querySelector(".todo-list");
@@ -6,18 +8,25 @@ todoAddButton.addEventListener("click", addTodo);
 function addTodo(event) {
     event.preventDefault();
 
-    const todoDiv = document.createElement('div');
-    todoDiv.classList.add("todo");
-    const newTodo = document.createElement('li');
-    newTodo.innerText = "buy a bigger brain";
-    newTodo.classList.add('todo-item');
-    todoDiv.appendChild(newTodo);
+    if (!items.includes(todoInput.value)) {
+        items.push(todoInput.value);
+        const todoDiv = document.createElement('div');
+        todoDiv.classList.add("todo");
+        const newTodo = document.createElement('li');
+        newTodo.innerText = todoInput.value;
+        newTodo.classList.add('todo-item');
+        todoDiv.appendChild(newTodo);
 
-    const completedButton = document.createElement("button");
-    completedButton.innerHTML = '<i class="far fa-check-square"></i>';
-    completedButton.classList.add("complete-btn");
-    todoDiv.appendChild(completedButton);
+        const completedButton = document.createElement("button");
+        completedButton.innerHTML = '<i class="far fa-check-square"></i>';
+        completedButton.classList.add("complete-btn");
+        todoDiv.appendChild(completedButton);
 
-    todoList.appendChild(todoDiv);
-
+        todoList.appendChild(todoDiv);
+        todoInput.value = "";
+    } else {
+        //might change this
+        window.alert(todoInput.value + " already exists!");
+    }
+    
 }
